f0c12dd0467420ece61b93c5895f01a602405145
Shem <oduor2shem@gmail.com>
2025-06-15 19:47:24 +0300

    Card removal

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,11 @@ import { TodoProvider } from './Context/TodoContext.jsx';
 import Home from './Pages/Home';
 import Navbar from './Components/Navbar';
 import './index.css';
-import './Components/Card.jsx'
+
 const App = () => {
   return (
     <TodoProvider>
       <Navbar />
-       <Card className="container mx-auto p-4">
-            <Home/>
-        </Card>
       <Home />
     </TodoProvider>
   );
